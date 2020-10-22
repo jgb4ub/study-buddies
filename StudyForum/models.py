633acf9afd20ed7e3702ff84fe4e5247540a0d16
sl8rn <sammyhl3027@gmail.com>
2020-10-22 18:58:18 -0400

    Fixed Primary key concerns

--- StudyForum/models.py
+++ StudyForum/models.py
@@ -18,3 +18,8 @@ class Post(models.Model):
     subject = models.CharField(max_length = 50)
     category = models.CharField(max_length = 20)
     content = models.CharField(max_length = 100)
+
+class PostRender(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
