b47c6aa4f65fc279cf7d773f533aa886d5db23bd
sl8rn <sammyhl3027@gmail.com>
2020-10-24 18:08:28 -0400

    Travis fix

--- teamInnovators/settings.py
+++ teamInnovators/settings.py
@@ -136,9 +136,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 
 SITE_ID = 1
