0ea50b7d5382ad93005da3673c23c768cdce9664
sl8rn <sammyhl3027@gmail.com>
2020-11-04 22:44:36 -0500

    Sammy Travis Test

--- teamInnovators/settings.py
+++ teamInnovators/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import os
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
@@ -140,7 +140,7 @@ AUTHENTICATION_BACKENDS = [
 #   import django_heroku
 #   django_heroku.settings(locals())
 
-#django_heroku.settings(locals(),test_runner=False)
+django_heroku.settings(locals(),test_runner=False)
 SITE_ID = 1
 
 
