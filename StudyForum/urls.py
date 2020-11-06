69fb47f4bd9ef09076efc2e2ba89e1586737bdff
ibykh13 <45862529+ibykh13@users.noreply.github.com>
2020-11-06 11:43:53 -0500

    tweaks for heroku

--- StudyForum/urls.py
+++ StudyForum/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('postsubmission/',views.postsubmit, name = 'postsubmit'),
     path('addpost/',views.addpost, name='addpost'),
     path('<int:post_id>/postpage/', views.postpage, name='postpage'),
-    path('<int:post_id>/postpage/', views.profilepage, name = 'profilepage')
+    path('profile/', views.profilepage, name = 'profilepage')
 ]
