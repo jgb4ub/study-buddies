048d7bf4c377096014713727bdd315dfa2df9982
ibykh13 <45862529+ibykh13@users.noreply.github.com>
2020-11-08 15:25:45 -0500

    Update views.py

--- StudyForum/views.py
+++ StudyForum/views.py
@@ -27,9 +27,12 @@ def postpage(request, post_id):
     return render(request, 'studyforum/postpage.html', {'post': post})
 
 def profilepage(request, id):
-    try{
-        profile = User.objects.get(userid=id)
-    } except {
-        profile = User(username="", firstname="Ibrahim", lastname="Hamdy", userid=id)
-    }
+    found = False
+    profileList = User.objects.all()
+    for i in profileList:
+        if i.userid == id:
+            profile = i
+            found = True
+    if not found:
+        profile = User(username="default", firstname="Ibrahim", lastname="Hamdy", userid=id)
     return render(request, 'studyforum/profilepage.html', {'profile': profile})
