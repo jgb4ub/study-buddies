e5965da71ede42bf622bb779c33f6a336133fea0
ibykh13 <45862529+ibykh13@users.noreply.github.com>
2020-11-08 20:20:33 -0500

    Update views.py

--- StudyForum/views.py
+++ StudyForum/views.py
@@ -11,15 +11,9 @@ def post(request):
     return render(request, 'studyforum/postings.html',context)
 
 def profile_page(request, username):
-    context = {'username': username,
-                'major': 'Computer Science',
-                'year': '2022'}
-    return render(request, 'studyforum/profile_page.html',context)
-
-
-def profilepage(request, id):
     profile = User.objects.get(userid=id)
     return render(request, 'studyforum/profile_page.html', {'profile': profile})
+    
 
 def editprofile(request, id):
     return render(request, 'studyforum/index.html')
