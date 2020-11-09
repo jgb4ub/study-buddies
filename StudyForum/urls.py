28f9855d22513c3beb8af63f197b9cfd40358db3
ibykh13 <45862529+ibykh13@users.noreply.github.com>
2020-11-08 20:24:30 -0500

    Update urls.py

--- StudyForum/urls.py
+++ StudyForum/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('sendchat/', views.messages, name='sendchat'),
     path('sendmessage/',views.sendmessage,name='sendmessage'),
     path('<str:user_id>/receivechat', views.messageReceived, name='receivechat'),
-    path('<str:username>/profilepage/',views.profile_page, name = 'profilepage'),
-    path('<int:id>/profilepage/', views.profilepage, name = 'profilepage')
+    path('<str:username>/profile_page/',views.profile_page, name = 'profile_page'),
+    path('<int:id>/profil_epage/', views.profile_page, name = 'profile_page')
 ]
