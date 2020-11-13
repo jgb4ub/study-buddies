c3d521dcf55ce237a686697dbcef61eff185647a
sl8rn <sammyhl3027@gmail.com>
2020-11-13 01:50:23 -0500

    Migrate fix

--- StudyForum/migrations/0001_initial.py
+++ StudyForum/migrations/0001_initial.py
@@ -11,8 +11,9 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('0012_alter_user_first_name_max_length'),
-        #('auth', '0012_alter_user_first_name_max_length'),
+       # ('auth'),
+       # ('0012_alter_user_first_name_max_length'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
