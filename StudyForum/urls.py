f651f0fa42ad14e6dfdf766e568ebea83ce0fab3
Davin Um <du5kx@virginia.edu>
2020-11-21 18:14:11 -0500

    forgot comma at views.py

--- StudyForum/urls.py
+++ StudyForum/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('courseenrollment/', views.courseenrollment, name='courseenrollment'),
     path('<int:id>/enrollcourse/', views.enrollcourse, name='enrollcourse'),
     path('editprofile/', views.editprofile, name='editprofile'),
-    path('<int:id>/profile_editor/', views.profile_editor, name='profile_editor')
+    path('<int:id>/profile_editor/', views.profile_editor, name='profile_editor'),
     path('<int:id>/delete/',views.delete_post,name='delete')
 ]
